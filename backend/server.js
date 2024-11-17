289c1746a1dd3e52334cb32d90e719676724e225
Arpita <raiarpita58@gmail.com>
2024-11-17 18:25:13 +0530

    Add MongoDB models and update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ app.use(express.json()); // Parse incoming JSON requests
 app.use('/uploads', express.static('uploads'));
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/shopify')
+
+
+
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
